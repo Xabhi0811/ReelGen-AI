199341a67b35a89fadab43a6dc3a88bee14b7bd3
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-23 00:08:16 +0530

    project data

--- Frontend/src/pages/Community.tsx
+++ Frontend/src/pages/Community.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import type { Project } from "../types"
+import { dummyGenerations } from "../assets/assets"
+import { Loader2Icon } from "lucide-react"
 
 
 const Community = () => {
-  return (
+  const [projects, setProjects] = useState<Project[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchProjects =async ()=>{
+    setTimeout(()=>{
+      setProjects(dummyGenerations);
+      setLoading(false)
+    },3000)
+  }
+
+  useEffect(()=>{
+   fetchProjects()
+  },[])
+
+
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader2Icon className="size-7 animate-spin text-indigo-400" />
+    </div>
+  ) : (
     <div>
-      
+
     </div>
   )
 }
