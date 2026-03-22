5466b40c4ed53bd6c580a93cfc618f431e86b724
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-22 22:53:18 +0530

    genetrated add

--- Frontend/src/pages/Genetator.tsx
+++ Frontend/src/pages/Genetator.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Title from "../components/Title"
 import UploadZone from "../components/uploadZone"
+import { Loader2Icon, RectangleHorizontalIcon, RectangleVerticalIcon, Wand2Icon } from "lucide-react"
+import { PrimaryButton } from "../components/Buttons"
 
 
 
@@ -33,7 +35,7 @@ const Genetator = () => {
         <Title heading="create In-Context Image" description="upload
         your model and product images to generate stunning UGC,
         short-form videos and social media posts"/>
-      </form>
+      
       <div className="flex gap-20 max-sm:flex-col items-start justify-between">
         {/* left col*/ }
         <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
@@ -45,7 +47,7 @@ const Genetator = () => {
         </div>
         {/* lright col*/ }
         <div className="w-full">
-            <div className="mb-4">
+            <div className="mb-4 text-gray-300">
               <label htmlFor="Name" className="block text-sm mb-4">Project Name</label>
               <input type="text" id="name" value={name} 
               onChange={(e)=>setName(e.target.value)} placeholder="Name your project"
@@ -74,9 +76,43 @@ const Genetator = () => {
               text-sm border-violet-200/10 focus:border-violet-500/50 outline-none
               resize-none transition-all" />
             </div>
+
+            <div className="mb-4 text-gray-300">
+              <label className="block text-sm mb-4">Aspect Ratio</label>
+              <div className="flex gap-3">
+                <RectangleVerticalIcon onClick={()=>setAspectRatio('9:16')}
+                  className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
+                  ring-transparent cursor-pointer${aspectRatio === '9:16' ? "ring-violet-500/50 bg-white/10": ''}`}/>
+
+                  <RectangleHorizontalIcon onClick={()=>setAspectRatio('16:9')}
+                  className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
+                  ring-transparent cursor-pointer${aspectRatio === '16:9' ? "ring-violet-500/50 bg-white/10": ''}`}/>
+              </div>
+            </div>
+
+             <div className="mb-4 text-gray-300">
+              <label htmlFor="userPrompt" className="block text-sm mb-4">
+                User Prompt<span className="text-xs text-violet-400"> (optional) </span></label>
+              <textarea id="userPrompt" rows={4}
+               value={userPrompt}
+              onChange={(e)=>setUserPrompt(e.target.value)}
+              placeholder="Describle how you want the narration to be." 
+              className="w-full bg-white/3 rounded-lg border-2 p-4
+              text-sm border-violet-200/10 focus:border-violet-500/50 outline-none
+              resize-none transition-all" />
+            </div>
+
         </div>
       </div>
-      
+      <div className="flex justify-center mt-10">
+        <PrimaryButton disabled={isGenerating} className="px-10 py-3 rounded-md
+         disabled:opacity-70 disabled:cursor-not-allowed">
+          {isGenerating ? (<><Loader2Icon className="size-5 animate-spin"/>
+             Generating.... </>
+          ) :(<> <Wand2Icon className="size-5"/>Generating Image</>)}
+        </PrimaryButton>
+      </div>
+      </form>
     </div>
   )
 }
