8b33302e9ec1d90fe5e1426bd19a5ce244286b2d
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-23 23:54:07 +0530

    done aaj ke liye

--- Frontend/src/pages/Result.tsx
+++ Frontend/src/pages/Result.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react"
+import type { Project } from "../types"
+import { dummyGenerations } from "../assets/assets"
+import { Loader2Icon } from "lucide-react"
 
 
 
 const Result = () => {
-  return (
-    <>
-    </>
+  const [project, setProjectData] = useState<Project>({}as Project)
+  const [loading, setLoading] = useState(true)
+  const [isGenerating, setIsGenerating] = useState(false)
+
+const fetchProjectData = async()=>{
+  setTimeout(()=>{
+    setProjectData(dummyGenerations[0])
+    setLoading(false)
+  },3000)
+}
+
+useEffect(()=>{
+  fetchProjectData()
+},[])
+
+
+
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo-size-9"/>
+
+    </div>
+  ) : (
+     
+    <div>
+
+    </div>
   )
 }
 
