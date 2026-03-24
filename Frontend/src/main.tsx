d792b198d36036faf6aca101583e12c12bf0de4e
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-25 02:31:11 +0530

    2:30 ho gya

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,11 +1,23 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-
+import { ClerkProvider } from '@clerk/react'
 import { BrowserRouter } from 'react-router-dom'
+import {dark} from "@clerk/themes"
 
-createRoot(document.getElementById('root')! as HTMLElement).render(
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+createRoot(document.getElementById('root')!).render(
+  <ClerkProvider appearance={{
+    theme: dark,
+    Variables: {
+        colorPrimary: "#4f39f6",
+        colorTextOnPrimaryBackground: "#ffffff"
+    }
+  }} publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </ClerkProvider>
 )
