761c7fa6767d36cd66e878840731323bbd397909
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-26 23:38:03 +0530

    backend chal rha hai

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,9 +1,12 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(clerkMiddleware())
 
 app.use(cors())
 app.use(express.json())
