a39825150330dd6abe2526b6f73ed613ddbeef7f
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-03-31 13:11:52 +0530

    Aws  Add

--- Frontend/src/configs/axios.ts
+++ Frontend/src/configs/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_BASEURL || 'https://reelgen-ai.duckdns.org'
 })
 
 export default api
